f51a6bb5454c14a2f5b3536c85f0887b8572554d
ermrema <pizzer20@gmail.com>
2022-09-10 07:21:12 -0700

    old

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,8 +4,8 @@
  *
  */
 
-int main(void)
+int main (void)
 {
-	puts("Programming is like building a multilingual puzzle\n");
-	return(0);
+	puts ("\"Programming is like building a multilingual puzzle\n");
+	return (0);
 }
