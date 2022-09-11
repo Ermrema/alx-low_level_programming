559996051aeaa34533bff47756b19fe1ed65515e
ermrema <pizzer20@gmail.com>
2022-09-11 13:06:37 +0000

    combination

---
+++ 0x01-variables_if_else_while/100-print_comb.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+/**
+ * main - mainf fuction
+ * Description: print combination of numbers
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i, j;
+
+	i = 48;
+	j = 48;
+
+	while (i < 58)
+	{
+		j = i + 1;
+		while (j < 58)
+		{
+			putchar(i);
+			putchar(j);
+
+			if (i < 56 || j < 57)
+			{
+				putchar(44);
+				putchar(10);
+			}
+			j++;
+		}
+		i++;
+	}
+	putchar (10);
+	return (0);
+}
