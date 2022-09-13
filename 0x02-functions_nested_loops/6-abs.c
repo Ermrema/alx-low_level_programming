76844cd021df09e424a410befc36262fcf987d66
ermrema <pizzer20@gmail.com>
2022-09-13 20:29:52 +0000

    sign of a number

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _abs - main function
+ * Description: prints the absolute value of an integer
+ * Return:0
+ */
+
+int _abs(int)
+{
+	int i, n;
+
+	i = n % 10;
+
+	_putchar('i');
+	return (0);
+}
