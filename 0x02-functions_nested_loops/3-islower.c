51210b5704243293b4bede28529e086fb20651b7
ermrema <pizzer20@gmail.com>
2022-09-13 19:51:28 +0000

    slower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,20 @@
+nclude "main.h"
+/**
+ * _islower - prints lowercase character
+ * Description:
+ * @c: An input character
+ * Return: return 1 if c is lowercase otherwise 0
+ */
+
+int _islower(int c)
+{
+	char i;
+	int e = 0;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		if (i == c)
+		e = 1;
+	}
+	return (e);
+}
