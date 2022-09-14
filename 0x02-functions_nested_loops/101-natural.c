10b021f8c8b7a9b540ff1ee97e26317e4ef0cf3d
ermrema <pizzer20@gmail.com>
2022-09-14 20:51:07 +0000

    natural fix

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -17,24 +17,11 @@ int main(void)
 
 	for (n = 1; n < 1024; n++)
 	{
-		if ((n % 3)  == 0 || (n % 5) ==0)
+		if ((n % 3)  == 0 || (n % 5) == 0)
 		{
 			sum1 = sum1 + n;
-	
-		}
-	
-	}
-	/**
-	for (j = 1; j < 1024; j++)
-	{
-		while ((j % 5) == 0)
-		{
-			sum2 = sum2 + j;
-			j++;
 		}
 	}
-	sum3 = sum1 + sum2;
-	*/
 	printf("%lu\n", sum1);
 	return (0);
 }
