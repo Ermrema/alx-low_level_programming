f3058b05b9a41b4d913f061bdd466ac30340f90e
ermrema <pizzer20@gmail.com>
2022-09-15 15:48:51 +0000

    most numbers

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -9,11 +9,12 @@ void print_most_numbers(void)
 {
 	int i;
 
-	i != (2 && 4);
-
-	for (i = 48; i < 58; i++)
+	while (i != 50 && i != 52)
 	{
-		_putchar(i);
+		for (i = 48; i < 58; i++)
+		{
+			_putchar(i);
+		}
+		_putchar('\n');
 	}
-	_putchar('\n');
 }
