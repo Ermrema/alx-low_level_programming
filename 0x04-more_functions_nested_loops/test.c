bdbbeab83af36a7e1d8ba6fcb7ac5bc0696aa45c
ermrema <pizzer20@gmail.com>
2022-09-15 18:49:45 +0000

    hot creation

---
+++ 0x04-more_functions_nested_loops/test.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ *  * more_numbers - main function
+ *   * Retun: 0
+ *    */
+
+int main(void)
+{
+	int i, j;
+	
+	while (j < 10)
+	{
+	      for (i = 0; i <= 14; i++)
+	          {
+	                   printf("%d", i);
+	          }
+	              j++;
+	             printf("\n");
+	 }
+}
