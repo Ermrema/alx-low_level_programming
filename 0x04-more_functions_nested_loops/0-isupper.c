075b208e1cc862481b75d6e78344bb77ef1d92f6
ermrema <pizzer20@gmail.com>
2022-09-15 14:58:05 +0000

    is digit

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -6,7 +6,7 @@
  * Return: 1 if upper, else retun 0
  */
 
-int _isupper(char c)
+int _isupper(int c)
 {
 	if (c >= 'A' && c <= 'Z')
 	{
---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _isdigit - main function
+ * @c: parameter
+ * Return: return 1 if true 0 is
+ */
+
+int _isdigit(int c)
+{
+	if (c >= 0 && c <= 9)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
