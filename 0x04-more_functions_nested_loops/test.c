4559c4a7673c6a1321bf7694c262c61243af440f
ermrema <pizzer20@gmail.com>
2022-09-16 18:48:54 +0000

    fix

--- 0x04-more_functions_nested_loops/test.c
+++ 0x04-more_functions_nested_loops/test.c
@@ -10,12 +10,13 @@ int main(void)
 {
 	int i, j, size;
 
-	size = 6;
+	size = 10;
 		 
   	for (i = 0; i < size; i++)
         {
-                for (j = 0; j < size; j++)
+                for (j = size - 1; j < size; j++)
                 {
+
                         printf("#");
 															                }
                 printf("\n");
