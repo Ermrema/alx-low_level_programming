9f2405385594d8d108f5fcb7022993f49a9beeea
ermrema <pizzer20@gmail.com>
2022-09-22 14:56:08 +0000

    fix

--- 0x05-pointers_arrays_strings/test.c
+++ 0x05-pointers_arrays_strings/test.c
@@ -1,15 +1,25 @@
 #include <stdio.h>
 
-int main(void)
+int main()
 {
-	int i = 50;
-	int *h;
+	int i;
+	int n;
 
-	h = &i;
+	printf(" the size of integer is %p\n", &i);
+	printf("size of a char is %p\n", &n);
 
-	printf("the value in address is %d\n", *h);
 
+	printf(" we are now storing the address of a variale in a pointer\n");
+
+	int k=10;
+	int j=1;
+
+	int *m=&k;
+	int *o=&j;
+	
+
+	printf("the value of the pointer is %p\n", m); 
+	*m=11;
+	printf("%d\n", k);
+ 	return (0);	
 }
-
-
-
