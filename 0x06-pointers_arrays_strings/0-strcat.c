3f81bf1956030a75ddee1d210129e35e8c2418aa
ermrema <pizzer20@gmail.com>
2022-09-22 14:52:51 +0000

    string concatenation

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _strcat - main function
+ * @dest: pointer parameter
+ * @src: pointer parameter
+ * Return:0
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	strcat(dest, src);
+	Return(*dest);
+}
---
+++ 0x06-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+This part we are going to go deeper in arrays pointers
